9d45c4dd797b4e65a3708f900d05ef93539dc4f5
tsakishii <ts.chris@protonmail.com>
2025-10-14 18:13:18 +0200

    remove: Second represent

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 func main() {
 	// create a multi dim slice
 	some := [][]int{{1,2,3}, {4,5,6}, {7, 8, 9}}
-	other := [][]int{{1,2,3}, {4,5,6}, {7, 8, 9}}
 
-	Represent(other)
 	Represent(some)
 }
