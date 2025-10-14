51545fbb809de5fe1ff19de76317baf4d0e22c35
tsakishii <ts.chris@protonmail.com>
2025-10-14 18:55:57 +0200

    add: channel for the Represent() func

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"encoding/json"
+)
+
 func main() {
 	// create a multi dim slice
 	some := [][]int{{1,2,3}, {4,5,6}, {7, 8, 9}}
 
-	Represent(some)
+	// put the slice in a chanel
+	ch := make(chan [][]int, 1)
+	go func() {
+		ch <- some
+		close(ch)
+	}()
+
+	mat:= <- ch
+	Represent(mat)
+
+
+	// funny rugby
+	foo := Foo{"Joe Marler", "Harlequins Tigers"}
+	but, _ := json.Marshal(foo)
+	fmt.Println(string(but))
+	json.Unmarshal(but, &foo)
 }
