1b9f4ef2c8fae2ea522c68d634f04f95ee727581
tsakishii <ts.chris@protonmail.com>
2025-10-29 18:17:58 +0100

    update: main function

--- main.go
+++ main.go
@@ -25,4 +25,17 @@ func main() {
 	but, _ := json.Marshal(foo)
 	fmt.Println(string(but))
 	json.Unmarshal(but, &foo)
+
+	arr := [...]int{1, 2, 3, 4}
+	sum := Sumar(&arr)
+
+	fmt.Printf("sum of arr: %d added from array {1,2,3,4}\n", sum)
+
+	arr1 := []int{1, 2, 3, 4}
+	sumarr1 := Sum(arr1)
+	fmt.Printf("sum of slice: %d\n", sumarr1)
+
+	// Test factorials
+	fmt.Println(Factorial(4.0))
+	fmt.Println(FactorialIter(4.0))
 }
